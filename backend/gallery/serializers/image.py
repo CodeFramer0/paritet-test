1cb37ea3d804761b9bfcae847cbe361229311872
CodeFramer <52332286+CodeFramer0@users.noreply.github.com>
2025-06-10 03:18:18 +0500

    migrate to extra fieds drf

--- backend/gallery/serializers/image.py
+++ backend/gallery/serializers/image.py
@@ -3,29 +3,14 @@ import uuid
 
 from django.core.files.base import ContentFile
 from rest_framework import serializers
-
+from drf_extra_fields.fields import Base64ImageField
 from gallery.models import Image
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    image_base64 = serializers.CharField(write_only=True, required=False)
+    image = Base64ImageField()
 
     class Meta:
         model = Image
-        fields = ["id", "image", "description", "image_base64"]
-        read_only_fields = ["id", "image"]
-
-    def create(self, validated_data):
-        base64_str = validated_data.pop("image_base64", None)
-        if base64_str:
-            validated_data["image"] = self._decode_base64_image(base64_str)
-        return super().create(validated_data)
-
-    def _decode_base64_image(self, base64_string: str) -> ContentFile:
-        try:
-            format, data = base64_string.split(";base64,")
-            ext = format.split("/")[-1]
-            file_name = f"{uuid.uuid4()}.{ext}"
-            return ContentFile(base64.b64decode(data), name=file_name)
-        except Exception:
-            raise serializers.ValidationError("Invalid base64 image format")
+        fields = ["id", "image", "description",]
+        read_only_fields = ["id",]
